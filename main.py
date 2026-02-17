d70171cdd15fd1fdc239fa0adb53d0a931810ad8
Abhishek Pandey <107237957+Abhishek7719@users.noreply.github.com>
2026-02-17 16:35:10 +0530

    Refactored main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import Driver
 
+def main():
+    try:
+        Driver.browser("chrome", "https://www.google.com/")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-
-
-
-Driver.browser("chrome","https://www.google.com/")
+if __name__ == "__main__":
+    main()
